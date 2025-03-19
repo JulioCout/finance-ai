c136495f16c230b0170c7e073da812f4fa8163ab
Julio Coutinho <juliosilvacoutinho@gmail.com>
2025-03-19 00:07:49 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,38 @@
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
+import { redirect } from "next/dist/server/api-utils";
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid grid-cols-2">
       {/* Esquerda */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <Image src="/logo.svg" width={173} height={39} alt="Finance AI" className="mb-8" />
-        <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
         <p className="text-muted-foreground mb-8">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-            <LogInIcon className="mr-2"/>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
             Fazer login ou criar conta
-        </Button>
+          </Button>
+        </SignInButton>
       </div>
       {/* Direita */}
       <div className="relative h-full">
