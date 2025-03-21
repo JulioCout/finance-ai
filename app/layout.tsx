41d5f9ae9331bf0cddc9f1aa37dc6440c4a44fb3
Julio Coutinho <juliosilvacoutinho@gmail.com>
2025-03-21 16:51:14 -0300

    feat: update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const mulish = Mulish({
+  subsets: ["latin-ext"]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${mulish.className} antialiased`}
       >
         <ClerkProvider>{children}</ClerkProvider>
       </body>
