c4b2d405eeee92befddda9340f905f9ba85819f6
Julio Coutinho <juliosilvacoutinho@gmail.com>
2025-03-25 14:43:51 -0300

    feat: criada pagina de transações

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Transaction, TransactionType } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/app/_components/ui/badge";
+import { CircleIcon } from "lucide-react";
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => {
+      if (transaction.type === TransactionType.DEPOSIT) {
+        return (
+          <Badge className="bg-muted text-primary hover:bg-muted font-bold">
+            <CircleIcon className="fill-primary mr-2" size={10} />
+            Depósito
+          </Badge>
+        );
+      }
+      if (transaction.type === TransactionType.EXPENSE) {
+        return (
+          <Badge className="bg-muted text-primary hover:bg-muted font-bold">
+            <CircleIcon className="fill-destructive mr-2" size={10} />
+            Despesa
+          </Badge>
+        );
+      }
+      if (transaction.type === TransactionType.INVESTMENT) {
+        return (
+          <Badge className="bg-muted text-primary hover:bg-muted font-bold">
+            <CircleIcon className="fill-primary mr-2" size={10} />
+            Investimento
+          </Badge>
+        );
+      }
+    },
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];
